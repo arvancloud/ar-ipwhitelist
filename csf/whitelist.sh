15db2ad62365ac9a10f898348bb88888bb0f796b
Mohammad Amin Taheri <aminium.atom@gmail.com>
2021-01-11 16:22:41 +0330

    Add ufw and csf script

---
+++ csf/whitelist.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+curl https://www.arvancloud.com/fa/ips.txt > ip.list
+
+ALLOW_IP_FILE=$1
+
+#allow ip in csf
+while IFS= read -r IP;
+do
+    echo $IP >> $ALLOW_IP_FILE;
+done < ip.list
+
+csf -r
+
+rm -f ip.list
---
+++ ufw/whitelist.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+curl https://www.arvancloud.com/fa/ips.txt > ip.list
+
+#allow ip in ufw
+while IFS= read -r IP;
+do
+    ufw allow from $IP;
+done < ip.list
+
+rm -f ip.list
