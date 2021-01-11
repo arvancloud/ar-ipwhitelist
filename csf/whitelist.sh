e04711aea1179eb7ab94cd2825f3cfeb8d619476
Mohammad Amin Taheri <aminium.atom@gmail.com>
2021-01-11 17:58:18 +0330

    fix:update scripts

--- csf/whitelist.sh
+++ csf/whitelist.sh
@@ -7,7 +7,7 @@ ALLOW_IP_FILE=$1
 #allow ip in csf
 while IFS= read -r IP;
 do
-    echo $IP >> $ALLOW_IP_FILE;
+    echo $IP >> $ALLOW_IP_FILE
 done < ip.list
 
 csf -r
--- ufw/whitelist.sh
+++ ufw/whitelist.sh
@@ -5,7 +5,7 @@ curl https://www.arvancloud.com/fa/ips.txt > ip.list
 #allow ip in ufw
 while IFS= read -r IP;
 do
-    ufw allow from $IP;
+    ufw allow from $IP
 done < ip.list
 
 rm -f ip.list
