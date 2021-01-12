0a5b4c2245ee42169f4ed76da8500832841a2500
Shaygan <shayganshok0@gmail.com>
2021-01-12 18:55:35 +0330

    Add the script

---
+++ src/ar-whitelister.sh
@@ -0,0 +1,41 @@
+#!/usr/bin/env bash
+
+# root access needed
+if [[ $EUID -ne 0 ]]; then
+  echo "You need to run this as root"
+  exit 1
+fi
+
+if [[ -z $1 ]]; then
+  echo "Select a firewall to add IPs:"
+  echo "   1) ufw"
+  echo "   2) csf"
+  read -r -p "Firewall: " option
+else
+  option=$1
+fi
+
+clear
+echo "Getting IPs..."
+IPs=$(curl -s https://www.arvancloud.com/fa/ips.txt)
+clear
+
+case "$option" in
+1 | ufw)
+  for IP in ${IPs}; do
+    sudo ufw allow from "$IP" to any
+  done
+  ;;
+2 | csf)
+  for IP in ${IPs}; do
+    sudo csf -a "$IP"
+  done
+  sudo csf -r
+  ;;
+*)
+  echo "Invalid firewall"
+  exit
+  ;;
+esac
+
+echo "DONE"
