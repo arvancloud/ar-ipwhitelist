73828e2312f97c380b5faed4a0a1b0037c04faf0
amir <amiralizadeh9480@gmail.com>
2021-01-12 23:08:10 +0330

    [WIP] Adding csf support

--- ar-ipwhitelist.sh
+++ ar-ipwhitelist.sh
@@ -13,31 +13,31 @@ abort()
 help()
 {
     echo -e "\033[1;36m"
-    echo "+------------------------------------------------------------------+"
-    echo "|     _    ______     ___    _   _    ____ _     ___  _   _ ____   |"
-    echo "|    / \  |  _ \ \   / / \  | \ | |  / ___| |   / _ \| | | |  _ \  |"
-    echo "|   / _ \ | |_) \ \ / / _ \ |  \| | | |   | |  | | | | | | | | | | |"
-    echo "|  / ___ \|  _ < \ V / ___ \| |\  | | |___| |__| |_| | |_| | |_| | |"
-    echo "| /_/   \_\_| \_\ \_/_/   \_\_| \_|  \____|_____\___/ \___/|____/  |"
-    echo "+------------------------------------------------------------------+"
+    echo "+-------------------------------------------------------------------+"
+    echo "|     _    ______     ___    _   _     ____ _     ___  _   _ ____   |"
+    echo "|    / \  |  _ \ \   / / \  | \ | |   / ___| |   / _ \| | | |  _ \  |"
+    echo "|   / _ \ | |_) \ \ / / _ \ |  \| |  | |   | |  | | | | | | | | | | |"
+    echo "|  / ___ \|  _ < \ V / ___ \| |\  |  | |___| |__| |_| | |_| | |_| | |"
+    echo "| /_/   \_\_| \_\ \_/_/   \_\_| \_|   \____|_____\___/ \___/|____/  |"
+    echo "+-------------------------------------------------------------------+"
     echo -e "\033[0m"
 
     echo "Arvan Cloud IP White List"
     echo ""
 
     if [[ $1 == "setup" ]]; then
-        echo "setup: Setup firewall to allow only Arvan Cloud IPs."
+        echo "setup: Setup firewall to allow Arvan Cloud CDN IPs only."
         echo ""
         echo "Usage: $0 setup --param=value ..."
         echo ""
         echo "Parameters:"
-        echo -e " --firewall|-f\t\tFirewall name. (default=ufw) (possible values= ufw)"
-        echo -e " --ports|-p\t\tAllow only specific ports. (Default=80,443)"
+        echo -e " --firewall|-f\t\tFirewall name. (Default=ufw) (Supported=ufw,csf,vmmanager)"
+        echo -e " --ports|-p\t\tAllowed ports[Just for ufw]. (Default=80,443)"
     else
         echo "Usage: $0 COMMAND"
         echo ""
         echo "Commands:"
-        echo -e " setup\t\t Setup firewall to allow CDN only IPs."
+        echo -e " setup\t\t Setup firewall to allow CDN IPs only."
     fi
     echo ""
 
@@ -45,28 +45,30 @@ help()
 }
 
 # Setup UFW firewall.
-setUfw()
+setupUfw()
 {
     local ips=$1
+    local ports=$2
 
     # Check ufw is installed
     if [ ! -x "$(command -v ufw)" ]; then
         abort "ufw firewall is not installed."
     fi
 
-    echo "Starting setup ufw firewall."
+    echo "Configuring ufw firewall started."
 
     # Enable ufw if is disabled
-    if [ $(ufw status|grep -qw inactive) ]; then
-        yes | ufw enable > /dev/null
+    if ufw status | grep -qw 'inactive'; then
+        echo "ufw is disabled. enabling ufw..."
+        yes | ufw enable > /dev/null || true
     fi
 
     ufw default deny incoming > /dev/null
     ufw allow 22 > /dev/null
 
     for ip in $ips; do
-        ufw allow from $ip port $ports proto tcp comment 'Arvan Cloud IP' > /dev/null
-        ufw allow from $ip port $ports proto udp comment 'Arvan Cloud IP' > /dev/null
+        ufw allow from $ip to any port $ports proto tcp comment 'Arvan Cloud IP' > /dev/null
+        ufw allow from $ip to any port $ports proto udp comment 'Arvan Cloud IP' > /dev/null
     done
 
     ufw reload > /dev/null
@@ -75,11 +77,55 @@ setUfw()
     exit 0
 }
 
+# Setup CSF firewall.
+setupCsf()
+{
+    local ips=$1
+    local ports=$2
+
+    ports="${ports/:/_}"
+
+    # Check csf is installed
+    if [ ! -x "$(command -v csf)" ]; then
+        abort "csf firewall is not installed."
+    fi
+
+    echo "Configuring csf firewall started."
+
+    sed -i '/##start-arvan-ip/,/##end-arvan-ip/d' /etc/csf/csf.allow
+    sed -i '/##start-arvan-ip/,/##end-arvan-ip/d' /etc/csf/csf.ignore
+
+    echo "##start-arvan-ip" >> /etc/csf/csf.allow
+    echo "##start-arvan-ip" >> /etc/csf/csf.ignore
+    for ip in $ips; do
+        echo "tcp|in|d=$ports|s=$ip # Arvan Cloud IP" >> /etc/csf/csf.allow
+        echo "udp|in|d=$ports|s=$ip # Arvan Cloud IP" >> /etc/csf/csf.allow
+        echo "$ip # Arvan Cloud IP" >> /etc/csf/csf.ignore
+    done
+    echo "##end-arvan-ip" >> /etc/csf/csf.allow
+    echo "##end-arvan-ip" >> /etc/csf/csf.ignore
+
+    csf -r > /dev/null
+
+    echo "csf configured successfully."
+    exit 0
+}
+
+# Setup VMManager firewall.
+setupVMManger()
+{
+    local ips=$1
+    local ports=$2
+
+    # TODO!
+}
+
 # Setup firewall
 setup()
 {
-    firewall="ufw"
-    ports="80,443"
+    local firewall="ufw"
+    local ports="80,443"
+
     while [ $# -gt 0 ]; do
         case "$1" in
         --help* | -h*)
@@ -100,7 +146,7 @@ setup()
         shift
     done
 
-    ips=""
+    local ips=""
     if [ -x "$(command -v curl)" ]; then
         ips=$(curl -s $ips_url)
     elif [ -x "$(command -v wget)" ]; then
@@ -111,10 +157,16 @@ setup()
 
     case $firewall in
     ufw)
-        setUfw "$ips"
+        setupUfw "$ips" "$ports"
+        ;;
+    csf)
+        setupCsf "$ips" "$ports"
+        ;;
+    vmmanger)
+        setupVMManger "$ips" "$ports"
         ;;
     *)
-        abort $"Unsupported Firewall.\nSupported Firewalls:\n * ufw"
+        abort $"Unsupported Firewall.\nSupported Firewalls:\n * ufw\n * csf\n * vmmanger"
         ;;
     esac
 }
